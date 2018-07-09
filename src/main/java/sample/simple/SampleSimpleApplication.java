4a53258701dfbb694281da14ff94a8446d31c248
shrikant <shrikant_soni@hotmail.com>
2018-07-10 00:27:21 +0530

    test

--- src/main/java/sample/simple/SampleSimpleApplication.java
+++ src/main/java/sample/simple/SampleSimpleApplication.java
@@ -1,33 +1,23 @@
 package sample.simple;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import sample.simple.service.HelloWorldService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
 @EnableAutoConfiguration
-public class SampleSimpleApplication implements CommandLineRunner {
+@SpringBootApplication
+public class SampleSimpleApplication  {
 
 	// Simple example shows how a command line spring application can execute an
 	// injected bean service. Also demonstrates how you can use @Value to inject
 	// command line args ('--name=whatever') or application properties
 
-	@Autowired
-	private HelloWorldService helloWorldService;
-
-	@Override
-	public void run(String... args) {
-		System.out.println(this.helloWorldService.getHelloMessage());
-		if (args.length > 0 && args[0].equals("exitcode")) {
-			throw new ExitException();
-		}
-	}
-
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		SpringApplication.run(SampleSimpleApplication.class, args);
 	}
 
